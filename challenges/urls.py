751fa9431bbf97072833c85e2f4137d833b635fb
Digvijay <digvijay.01.dc@gmail.com>
2023-09-15 22:19:05 -0700

    adding dynamic urls

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.january),
-    path("february", views.february)
+    # path for static urls
+    # path("january", views.january),
+    # path("february", views.february)
+
+    #path for dynamic urls:
+    path("<month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
+# functions for static urls
+# def january(request):
+#     return HttpResponse("This is first month of the year")
 
-def january(request):
-    return HttpResponse("This is first month of the year")
+# def february(request):
+#     return HttpResponse("This is second month of the year")
 
-def february(request):
-    return HttpResponse("This is second month of the year")
+
+# function for dynamic urls:
+def monthly_challenge(reques, month):
+    challengeText = None
+    if month == 'january':
+        challengeText = "This is january"
+    elif month == "feburary":
+        challengeText = 'This is february'
+    elif month == 'march':
+        challengeText = "This is march"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challengeText)
